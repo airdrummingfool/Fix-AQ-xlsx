5f07222debe40f8dfc87f8ff21f057e4be2e4436
Tommy Goode <tommy@irdequipment.com>
2015-01-23 16:53:29 -0600

    Using more environmental variables instead of hard-coding, and doing (almost) everything by absolute reference.

--- fix_xlsx.bat
+++ fix_xlsx.bat
@@ -1,21 +1,38 @@
-cd
 @echo off
-: make temp dir
-mkdir temp
+REM - File: fix_xlsx.bat
+REM - Description: Fix corrupted .xlsx file exported from AutoQuotes
+REM - Author: Tommy Goode
+REM - Copyright 2015 International Restaurant Distributors, Inc.
+
+: set up temp vars
+set "script_dir=%~dp0"
+set "tempdir=%script_dir%temp"
+set "tempfile=%tempdir%\temp.zip"
+set "tempfixed=%tempdir%\tempfixed.zip"
+set "file_dir=%~dp1"
+set "fixedfilename=fixed.xlsx"
+set "fixedfile=%file_dir%%fixedfilename%"
+echo Fixed file will be: %fixedfile%
+
+: make temp dir and ensure it is empty
+if exist "%tempdir%\" rd /s /q %tempdir%
+mkdir "%tempdir%"
 
 : copy local, make .zip
-copy /Y %1 temp\temp.zip
+copy /Y %1 %tempdir%\temp.zip
 
 : cd into to tmp dir
-cd temp
+cd "%tempdir%"
 
 : unzip xlsx file
-..\izarc\izarce.exe -e -d -o temp.zip
-del temp.zip
-cd
-..\izarc\IZARCC.exe -a -r -p -x"tempfixed.zip" ..\temp\tempfixed.zip
-copy /Y tempfixed.zip ..\fixed.xlsx
-cd ..
-rd /S /Q temp
+%script_dir%\izarc\izarce.exe -e -d -o %tempfile%
+del "%tempfile%"
+%script_dir%\izarc\IZARCC.exe -a -r -p %tempfixed%
+
+copy /Y "%tempfixed%" "%fixedfile%"
+del "%tempfixed%"
+
 pause
-start fixed.xlsx
+: for some reason we can't open the file with an absolute path
+cd "%file_dir%"
+start %fixedfilename%
