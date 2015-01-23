89ce5b4701383c779d4159a99f358cac3bac7243
Tommy Goode <tommy@irdequipment.com>
2015-01-23 17:02:03 -0600

    Piping all misc output to nul to simplify what the user sees.

--- fix_xlsx.bat
+++ fix_xlsx.bat
@@ -12,6 +12,7 @@ set "tempfixed=%tempdir%\tempfixed.zip"
 set "file_dir=%~dp1"
 set "fixedfilename=fixed.xlsx"
 set "fixedfile=%file_dir%%fixedfilename%"
+echo.
 echo Fixed file will be: %fixedfile%
 
 : make temp dir and ensure it is empty
@@ -19,20 +20,23 @@ if exist "%tempdir%\" rd /s /q %tempdir%
 mkdir "%tempdir%"
 
 : copy local, make .zip
-copy /Y %1 %tempdir%\temp.zip
+copy /Y %1 %tempdir%\temp.zip > nul
 
 : cd into to tmp dir
 cd "%tempdir%"
 
 : unzip xlsx file
-%script_dir%\izarc\izarce.exe -e -d -o %tempfile%
+%script_dir%\izarc\izarce.exe -e -d -o %tempfile% > nul
 del "%tempfile%"
-%script_dir%\izarc\IZARCC.exe -a -r -p %tempfixed%
+%script_dir%\izarc\IZARCC.exe -a -r -p %tempfixed% > nul
 
-copy /Y "%tempfixed%" "%fixedfile%"
-del "%tempfixed%"
+copy /Y "%tempfixed%" "%fixedfile%" > nul
+del "%tempfixed%" > nul
 
+echo.
+echo Done!
 pause
+
 : for some reason we can't open the file with an absolute path
 cd "%file_dir%"
 start %fixedfilename%
